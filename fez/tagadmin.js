5b8da66b9cf02fb2af5c8e0c514ff0621a516423
Next5x/timnasa <botuse41@gmail.com>
2025-06-27 05:36:34 +0200

    Update tagadmin.js

--- fez/tagadmin.js
+++ fez/tagadmin.js
@@ -1,14 +1,14 @@
-const { zokou } = require("../framework/zokou")
+const { timoth } = require("../timnasa/timoth")
 //const { getGroupe } = require("../bdd/groupe")
 const { Sticker, StickerTypes } = require('wa-sticker-formatter');
 const {ajouterOuMettreAJourJid,mettreAJourAction,verifierEtatJid} = require("../bdd/antilien")
 const {atbajouterOuMettreAJourJid,atbverifierEtatJid} = require("../bdd/antibot")
 const fs = require("fs-extra");
 const conf = require("../set");
-const { default: axios } = require('axios');
+//const { default: axios } = require('axios');
 
 
-zokou({ nomCom: "tagadmin", categorie: 'Group', reaction: "🪰" }, async (dest, zk, commandeOptions) => {
+timoth({ nomCom: "tagadmin", categorie: 'Group', reaction: "🪰" }, async (dest, zk, commandeOptions) => {
 
   const { ms, repondre, arg, verifGroupe, nomGroupe, infosGroupe, nomAuteurMessage, verifAdmin, superUser } = commandeOptions;
 
