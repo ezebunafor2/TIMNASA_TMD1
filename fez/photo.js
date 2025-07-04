a492ab593a82abb8437b741a972fe231becee025
Next5x/timnasa <botuse41@gmail.com>
2025-07-04 15:30:46 +0300

    Update photo.js

--- fez/photo.js
+++ fez/photo.js
@@ -1,12 +1,20 @@
-const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
 const { timoth } = require("../timnasa/timoth");
-const traduire = require("../timnasa/traduction");
-const { downloadMediaMessage,downloadContentFromMessage } =  require('@whiskeysockets/baileys');
-const fs =require("fs-extra") ;
-const axios = require('axios');  
-const FormData = require('form-data');
-const { exec } = require("child_process");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../timnasa/dl/Function");
+const { default: axios } = require('axios');
 
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+   }
 
 
 async function uploadToTelegraph(Path) {
