8cec0ac73a1764b43149a004c6573426de33d9b1
Next5x/timnasa <botuse41@gmail.com>
2025-07-04 16:02:22 +0300

    Update ytst.js

--- fez/ytst.js
+++ fez/ytst.js
@@ -1,9 +1,21 @@
 const { timoth } = require("../timnasa/timoth");
-const { getytlink, ytdwn } = require("../timnasa/ytdl-core");
-const yts = require("yt-search");
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-  
+const moment = require("moment-timezone");
+const { getBuffer } = require("../timnasa/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+}
+
 timoth({ nomCom: "yts", categorie: "Search", reaction: "🐅" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, arg } = commandeOptions;
   const query = arg.join(" ");
