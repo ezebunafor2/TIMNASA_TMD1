9e7d5bfbdd4e44af28f93f3bc0bf2038e4361bf7
Next5x/timnasa <botuse41@gmail.com>
2025-07-05 04:55:32 +0300

    Update check-online.js

--- fez/check-online.js
+++ fez/check-online.js
@@ -1,7 +1,24 @@
-const { timoth } = require('../timnasa/timoth');
+const { timoth } = require("../timnasa/timoth");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../timnasa/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+//const { timoth } = require('../timnasa/timoth');
 
 timoth({
-    pattern: "online",
+    pattern: "listonline",
     alias: ["whosonline", "onlinemembers"],
     desc: "Check who's online in the group (Admins & Owner only)",
     category: "main",
