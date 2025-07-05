37301ce422046b35ace53c3dc582ff05067b4729
Next5x/timnasa <botuse41@gmail.com>
2025-07-05 05:34:06 +0300

    Update vvck.js

--- fez/vvck.js
+++ fez/vvck.js
@@ -1,20 +1,15 @@
-const { timoth } = require("../timnasa/timoth");
-const moment = require("moment-timezone");
-const { getBuffer } = require("../timnasa/dl/Function");
+const { timoth } = require("../timnasa/timoth")
+//const { getGroupe } = require("../database/groupe")
+const { Sticker, StickerTypes } = require('wa-sticker-formatter');
+const {ajouterOuMettreAJourJid,mettreAJourAction,verifierEtatJid} = require("../data/antilien")
+const {atbajouterOuMettreAJourJid,atbverifierEtatJid} = require("../data/antibot")
+const { search, download } = require("aptoide-scraper");
+const fs = require("fs-extra");
+const conf = require("../set");
 const { default: axios } = require('axios');
-
-const runtime = function (seconds) { 
- seconds = Number(seconds); 
- var d = Math.floor(seconds / (3600 * 24)); 
- var h = Math.floor((seconds % (3600 * 24)) / 3600); 
- var m = Math.floor((seconds % 3600) / 60); 
- var s = Math.floor(seconds % 60); 
- var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
- var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
- var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
- var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
- return dDisplay + hDisplay + mDisplay + sDisplay; 
- } 
+const {ajouterUtilisateurAvecWarnCount , getWarnCountByJID , resetWarnCountByJID} = require('../data/warn')
+const s = require("../set")
+//const { uploadImageToImgur } = require('../timnasa/imgur');
 
 
 timoth({ nomCom: 'vcf_10',
