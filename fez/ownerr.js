2ccfa81eb61cd5d709939663fa5e0ca259f51599
Next5x/timnasa <botuse41@gmail.com>
2025-07-06 18:59:46 +0300

    Update ownerr.js

--- fez/ownerr.js
+++ fez/ownerr.js
@@ -1,16 +1,21 @@
-const { timoth } = require('../timnasa/timoth');
-const axios = require("axios")
-let { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
-const {isUserBanned , addUserToBanList , removeUserFromBanList} = require("../bdd/banUser");
-const  {addGroupToBanList,isGroupBanned,removeGroupFromBanList} = require("../bdd/banGroup");
-const {isGroupOnlyAdmin,addGroupToOnlyAdminList,removeGroupFromOnlyAdminList} = require("../bdd/onlyAdmin");
-const {removeSudoNumber,addSudoNumber,issudo} = require("../bdd/sudo");
-//const conf = require("../set");
-//const fs = require('fs');
-const sleep =  (ms) =>{
-  return new Promise((resolve) =>{ setTimeout (resolve, ms)})
-  
-  } ;
+const { timoth } = require("../timnasa/timoth");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../timnasa/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
 
 
   timoth({ nomCom: "telesticker", categorie: "Mods" }, async (dest, zk, commandeOptions) => {
