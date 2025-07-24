8778161f3b8b81e2a68b21da23f9a0f69e8d2873
FredieTech <frediezra60@gmail.com>
2025-07-24 23:02:40 +0300

    j

--- fez/a__ntitag.js
+++ fez/a__ntitag.js
@@ -6,7 +6,7 @@ const antiTagStatus = new Map();
 timoth({
   nomCom: "antitag",
   categorie: "Group",
-  reaction: "⚠️",
+  reaction: "🔞",
   fromMe: true, // Only works when used by admin/owner
   desc: "Protect owner from being tagged in groups"
 }, async (dest, zk, commandeOptions) => {
